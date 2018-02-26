e15107b60b3e1d1436b0dfc47e213ba4c3fd9e0f
rodrigogregorioneri <rodrigogregorioneri>
2018-02-26 02:51:38 +0000

    estrutura de test se tudo der certo termino amanhã cedo

---
+++ src/main/java/app/api/test/PedidoTeste.java
@@ -0,0 +1,28 @@
+package app.api.test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+ 
+public class PedidoTeste {
+
+
+  @Before
+  public void iniciador() {
+
+  }
+
+
+  @Test
+  public void Test() {
+
+  }
+
+
+  @After
+  public void aposTest() {
+     
+  }
+
+}
