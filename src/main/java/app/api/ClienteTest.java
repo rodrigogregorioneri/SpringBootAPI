1d97b4d9092a26aa4736c2399833e877dda36b99
rodrigogregorioneri <rodrigogregorioneri>
2018-02-26 12:15:21 +0000

    adicionando as classes de testes novamente

---
+++ src/main/java/app/api/ClienteTest.java
@@ -0,0 +1,27 @@
+package app.api.test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+ 
+public class ClienteTest {
+
+
+ @Before
+  public void iniciador() {
+
+  }
+
+
+  @Test
+  public void Test() {
+
+  }
+
+
+  @After
+  public void aposTest() {
+     
+  }
+
+}
